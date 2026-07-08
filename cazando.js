a83df597eaa80bad8f34d72a9770aa0707b22e0d
ElFernan17 <ferx77silva2@gmail.com>
2026-07-08 14:21:32 -0500

    añadir variables de posiciones, comida, gato, funcion iniciarJuego

--- cazando.js
+++ cazando.js
@@ -1,7 +1,29 @@
 let canvas = document.getElementById("areaJuego");
 let context = canvas.getContext("2d");
 
-function graficarGato(){
+//constantes
+const anchoGato = 20
+const altoGato = 40
+const anchoComida = 10
+const altoComida = 10
+
+//variables
+let gatoX = canvas.width/2
+let gatoY = canvas.height/2
+let comidaX = 490
+let comidaY = 490
+
+function dibujarGato(){
     context.fillStyle = "orange"
-    context.fillRect(canvas.width/2,canvas.height/2,15,30)
+    context.fillRect(gatoX,gatoY,anchoGato,altoGato)
 } 
+
+function dibujarComida(){
+    context.fillStyle = "red"
+    context.fillRect(comidaX,comidaY,anchoComida,altoComida)
+}
+
+function iniciarJuego(){
+    dibujarComida();
+    dibujarGato();
+}
